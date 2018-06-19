e868412af81c8466bfd45557fc08ce2b3848707b
Thaya <hayafluss@gmail.com>
2018-06-19 23:37:37 +0900

    small debug

--- cython_pyx_fde_sc_c2.pyx
+++ cython_pyx_fde_sc_c2.pyx
@@ -1,7 +1,5 @@
 cimport numpy as np
 
-cdef extern from "cython_fde_sc_c2.h":
-    double complex c_algo(double complex *arr_a, double complex *arr_b, int size_a, int size_b)
 
 cdef extern from "cython_fde_sc_c2.h":
     int c_cauchy_2by2(double complex* Z, double complex* o_G, int max_iter, float thres, double sigma, int p_dim, int dim, long* o_forward_iter)
@@ -11,10 +9,6 @@ cdef extern from "cython_fde_sc_c2.h":
     int c_cauchy_subordination(double complex* B, double complex* o_omega,double complex* o_G_sc,int max_iter,double thres, \
     double sigma, int p_dim, int dim, long* o_forward_iter,double* a,  double complex* o_omega_sc)
 
-
-
-def cy_algo(np.ndarray[double complex, ndim=1] arr_a, np.ndarray[double complex, ndim=1] arr_b):
-    return c_algo(&arr_a[0], &arr_b[0], len(arr_a), len(arr_b))
 
 
 
--- test_cython/cython_c_code.h
+++
@@ -1,4 +0,0 @@
-#ifndef CYTHON_CODE
-#define CYTHON_CODE
-long c_algo(long*, long*, int, int);
-#endif
