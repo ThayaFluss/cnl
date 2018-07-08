569d4b80beb9020cb749bd2a0ba8420cb61c451f
Thaya <hayafluss@gmail.com>
2018-07-08 21:10:04 +0900

    modify tests

--- src/tests/test_psvd.py
+++ src/tests/test_psvd.py
@@ -5,13 +5,13 @@ from random_matrices import *
 
 class TestPSVD(unittest.TestCase):
     def test_psvd_cnl(self):
-        p = 60
-        d = 60
+        p = 50
+        d = 50
         sigma = 0.2
         zero_dim = 40
         min_singular = 0.3
         sample_mat = random_from_diag(p,d,zero_dim, min_singular)
         sample_mat += sigma*Ginibre(p,d)
         sample_mat *= 2
-        U,D, V = prob_svd_cnl(sample_mat)
+        U,D, V = psvd_cnl(sample_mat)
         #import pdb; pdb.set_trace()
